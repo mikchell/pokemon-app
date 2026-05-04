4a793974f5dd116d78f91c16e7030802d146911f
Jenny-high <okihomotmik@gmail.com>
2026-05-04 13:31:42 +0900

    chore: プロジェクト構成を更新

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  useEffect
+
+  return (
+    <div className="App"></div>
+  );
+};
+
+export default App;
